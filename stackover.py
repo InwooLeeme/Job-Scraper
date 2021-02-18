6b6d43fa1fcace9a592fea9d20bd14ec6dd1f6b7
ItsmeLee <arsmagica90@gmail.com>
2021-02-18 20:47:33 +0900

    Extract apply link
    Extract apply link from html source.

--- stackover.py
+++ stackover.py
@@ -8,6 +8,12 @@ def extract_stack():
     jobs = []
     r = requests.get(stackover_url)
     html = BeautifulSoup(r.text, "html.parser")
+    table = html.find("div", class_="js-search-results")
+    job_table = table.find("div", class_="grid--cell")
+    search_result = job_table.find_all("div", class_="-job")
+    for each_job in search_result:
+        apply_link = each_job.find("a", class_="s-link")
+        print(apply_link)
 
 
 extract_stack()
