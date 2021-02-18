0666f31ab493f94b97129ce669bf6a9e71525dd9
ItsmeLee <arsmagica90@gmail.com>
2021-02-18 22:03:34 +0900

    Import a function from stackover.py
    Import extract stackover function from stackover.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from wwr import extract_wwr
 from remoteok import extract_remoteok
+from stackover import extract_stackover
 
 wwr_result = extract_wwr()
 
 remoteok_result = extract_remoteok()
 
-print(remoteok_result)
+stackover_result = extract_stackover()
+
+print(stackover_result)
