9beb7e55ffdb5eb4929d9ad7dd50257b03de49a7
ItsmeLee <arsmagica90@gmail.com>
2021-02-19 19:11:53 +0900

    Hosting template
    Hosting template using flask.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from wwr import extract_wwr
 from remoteok import extract_remoteok
 from stackover import extract_stackover
+from flask import Flask, render_template, redirect
+
+app = Flask("JobScrper")
 
 wwr_result = extract_wwr()
 
@@ -8,4 +11,10 @@ remoteok_result = extract_remoteok()
 
 stackover_result = extract_stackover()
 
-print(stackover_result)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+app.run(host="localhost")
